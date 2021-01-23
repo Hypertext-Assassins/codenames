a8c251a774c08db12258840f98dc7c607191c2a4
Skylar Wong <li.q.wong@gmail.com>
2021-01-23 03:36:53 -0600

    create basic Card component, add Link from landing pg to /game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import * as wordsAPI from '../services/words'
+import Card from '../components/Card'
 
 const Game = () => {
     
@@ -10,7 +11,12 @@ const Game = () => {
         <>
         <h1>Game</h1>
         {words.map(el => 
-            <p>{el.word}</p>
+            <Card 
+                key={el.id}
+                id={el.id}
+                word={el.word}
+                color={el.color}
+            />
         )}
         
         </>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const LandingPage = () => {
     return (  
+        <>
         <h1>Codenames Landing Page</h1>
+        <button><Link exact to="/game">Play Codenames</Link></button>
+        </>
     );
 }
  
