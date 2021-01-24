64a2ce826d6f9bf547c06b0b0bc07645cab239d1
Alanna Celentano <celentanoad91@gmail.com>
2021-01-24 00:40:31 -0600

    set up server.js and room input on landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
+
+    const [user, setUser] = useState('');
+    const [room, setRoom] = useState('');
+
     return (  
         <>
         <h1>Codenames Landing Page</h1>
-        <button><Link exact to="/game">Play Codenames</Link></button>
+        <input placeholder="username" type="text" onChange={(e) => setUser(e.target.value)}/>
+        <input placeholder="room" type="text" onChange={(e) => setRoom(e.target.value)}/>
+        <button><Link exact to={`/game?room=${room}`}>Play Codenames</Link></button>
         </>
     );
 }
