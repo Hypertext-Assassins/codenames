e02f3e366d32cd8e00a2432532bc18a0eea1afa1
Skylar Wong <li.q.wong@gmail.com>
2021-01-26 16:57:10 -0600

    update score component spacing

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -6,50 +6,47 @@ const Score = (props) => {
 
     return (  
         <>
-
-        <Box className="score-board" 
-            
-            borderRadius="lg" 
-            overflow="hidden"
+            <Box className="score-board"  
+                borderRadius="lg" 
+                overflow="hidden"
             >
-          <p>Score component</p>
-        { props.winner ?
-            <p>Congrats {props.winner} has won!</p>    
-        :
-        <Spacer />
-        <Flex>
-            <Box 
-            bg="red.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="lg"
-            textTransform="uppercase"
-            m="50px"
-            p="10px"
-            boxShadow="dark-lg"
-            px="4" 
-            >
-
-        <p>red: {props.score["red.500"]} </p>
-        
+                <p>Score component</p>
+                { props.winner ?
+                    <p>Congrats {props.winner} has won!</p>    
+                :
+                    <></>
+                }
+                <Spacer />
+                <Flex>
+                    <Box 
+                        bg="red.500"
+                        fontWeight="semibold"
+                        letterSpacing="wide"
+                        fontSize="lg"
+                        m="50px"
+                        p="10px"
+                        boxShadow="dark-lg"
+                        px="4"
+                        borderRadius="md" 
+                    >
+                        RED: {props.score["red.500"]}
+                    </Box>
+                    <Spacer />
+                    <Box 
+                        bg="blue.400"
+                        fontWeight="semibold"
+                        letterSpacing="wide"
+                        fontSize="lg"
+                        boxShadow="dark-lg"
+                        p="10px"
+                        m="50px"
+                        px="4"
+                        borderRadius="md"
+                    >
+                        BLUE: {props.score["blue.400"]}
+                    </Box>
+                </Flex>
             </Box>
-            <Spacer />
-            <Box 
-            bg="blue.400"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="lg"
-            textTransform="uppercase"
-            boxShadow="dark-lg"
-            p="10px"
-            m="50px"
-            px="4">
-            <p>blue: {props.score["blue.400"]} </p>
-            </Box>
-            </Flex>
-        </Box>
-
-        
         </>
     );
 }
